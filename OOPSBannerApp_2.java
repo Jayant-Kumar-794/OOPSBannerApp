eeae5c58428178405e241cb1cfd3adad57d58d00
Jayant-Kumar-794 <jk4288@srmist.edu.in>
2026-02-16 07:56:02 +0530

    initial commit

---
+++ OOPSBannerApp_2.java
@@ -0,0 +1,18 @@
+/**
+ * @author: Jayant Kumar
+ * @version: 1.0
+ */
+
+public class OOPSBannerApp_2 {
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****    ******     *****  ");
+        System.out.println("*     *   *     *   *     *   *     * ");
+        System.out.println("*     *   *     *   *     *   *       ");
+        System.out.println("*     *   *     *   ******     *****  ");
+        System.out.println("*     *   *     *   *              *  ");
+        System.out.println("*     *   *     *   *        *     *  ");
+        System.out.println(" *****     *****    *         *****   ");
+
+    }
+}
