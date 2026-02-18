8fee2cd74c98661199bf330eb8c70ad5fc790203
Jayant-Kumar-794 <jk4288@srmist.edu.in>
2026-02-18 15:48:13 +0530

    Coded to Render OOPSBanner using Array Init to Console

---
+++ OOPSBannerApp_5.java
@@ -0,0 +1,17 @@
+/**
+ * @author: Jayant Kumar
+  *
+ * @version: 1.0
+ */
+
+public class OOPSBannerApp_5 {
+    public static void main(String[] args) {
+        System.out.println(String.join("\n", new String[] {
+            "  ***   ***  ****  **** ",
+            " *   * *   * *   * *  ",
+            " *   * *   * ****  *** ",
+            " *   * *   * *       * ",
+            "  ***   ***  *    **** "
+        }));
+    }
+}
